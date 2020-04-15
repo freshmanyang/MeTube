a81f0c29f12a14a095d8ee90d2f331d9bc81e99f
alanyang <chingyy@g.clemson.edu>
2020-04-14 22:35:11 -0400

    1.Add remove friend function 2.Delete video also delete video_keyword table 3.Remove unnecessary blank line 4.Format files(ctrl+alt+l)

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
 <?php require_once("./includes/header.php"); ?>
 <?php require_once("./includes/nav.php"); ?>
 <?php require_once('./includes/class/showAllVideo.php'); ?>
-
 <main class="main-section-container" id="main">
     <div class="main-content-container">
         <?php
         $showAllVideo = new showAllVideo($conn);
-        if(isset($usernameLoggedIn)){
+        if (isset($usernameLoggedIn)) {
             echo '<div id="main-video-container"> 
                   <div id ="welcomemessage">Welcome to MeTube,'
-                .ucfirst($usernameLoggedIn).'<br>';
-
+                . ucfirst($usernameLoggedIn) . '<br>';
         }
-        if(isset($_GET['category'])){
-            echo 'You are under category -'.$_GET['category'];
+        if (isset($_GET['category'])) {
+            echo 'You are under category -' . $_GET['category'];
         }
-
         ?>
     </div> <!--   welcomemessage div end-->
     <!--category browse button-->
     <div class="btn-group">
-        <button type="button" id="category" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <button type="button" id="category" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
             <?php
-            if (isset($_GET['category'])){
-            echo $_GET['category'];
-            }
-            else{
+            if (isset($_GET['category'])) {
+                echo $_GET['category'];
+            } else {
                 echo 'Category';
             }
             ?>
@@ -40,37 +37,32 @@
     <!--category button event handler-->
     <script>
         $(document).ready(function () {
-            $('#categoryList a').on('click', function(){
+            $('#categoryList a').on('click', function () {
                 var category = ($(this).text());
-                var href = "index.php?category="+category;
-                if(category === 'All'){
-                     href = "index.php";
-
+                var href = "index.php?category=" + category;
+                if (category === 'All') {
+                    href = "index.php";
                 }
                 // alert(category);
-
                 window.location.assign(href);
             });
         });
     </script>
-
     <div id="showAllVideo">
         <?php
-        If(isset($_GET['category'])){
+        If (isset($_GET['category'])) {
             echo '<div id="categoryvideopage">';
-            if(is_array($showAllVideo->getCategoryVideoswithBlock($usernameLoggedIn,$_GET['category']))){
-                $videoswithblock = $showAllVideo->getCategoryVideoswithBlock($usernameLoggedIn,$_GET['category']);
+            if (is_array($showAllVideo->getCategoryVideoswithBlock($usernameLoggedIn, $_GET['category']))) {
+                $videoswithblock = $showAllVideo->getCategoryVideoswithBlock($usernameLoggedIn, $_GET['category']);
 //                print_r($videoswithblock);
-                foreach($videoswithblock as $value){
+                foreach ($videoswithblock as $value) {
                     echo $value;
                 }
-
-            }
-            else{
-            echo $showAllVideo->getCategoryVideoswithBlock($usernameLoggedIn,$_GET['category']);
+            } else {
+                echo $showAllVideo->getCategoryVideoswithBlock($usernameLoggedIn, $_GET['category']);
             }
             echo '</div>';
-        }else{
+        } else {
             echo '<div id ="allvideopage"> </div>';
             echo '<div id="page-nav">
            <nav aria-label="Page navigation">
@@ -79,46 +71,41 @@
        </div>';
         }
         ?>
-
     </div>
     </div>
     <!--   main-video-container div end-->
-
     <script type="text/javascript">
         $(function () {
             // setup how many records in one page for channel and videos sorting tab
             var recordsPerPage = 8;
             $.ajax({
-                type:'POST',
-
-                url:'showallvideoprocess.php',
-                data:{
-                    showallvideo:"1",
-                    loginUser:'<?php echo $usernameLoggedIn ?>'
-
+                type: 'POST',
+                url: 'showallvideoprocess.php',
+                data: {
+                    showallvideo: "1",
+                    loginUser: '<?php echo $usernameLoggedIn ?>'
                 },
-                datatype:'json',
-                success:function(result){
+                datatype: 'json',
+                success: function (result) {
                     final = JSON.parse(result);
                     datalength = final.length;
                     window.pagObj = $('#pagination').twbsPagination({
                         // totalPages plugin if you want display 4 records at one page, total pages= total data /4
-                        totalPages: (datalength % recordsPerPage) ?  (datalength /recordsPerPage) + 1: datalength /recordsPerPage,
+                        totalPages: (datalength % recordsPerPage) ? (datalength / recordsPerPage) + 1 : datalength / recordsPerPage,
                         visiblePages: 5,
                         onPageClick: function (event, page) {
-                            document.getElementById("allvideopage").innerHTML ="";
+                            document.getElementById("allvideopage").innerHTML = "";
                             // console.info(page + ' (from options)');
-                            for ($i = recordsPerPage; $i >0 ; $i--) {
-                                if ( !(final[page * recordsPerPage - $i] == null)){
+                            for ($i = recordsPerPage; $i > 0; $i--) {
+                                if (!(final[page * recordsPerPage - $i] == null)) {
                                     document.getElementById("allvideopage").innerHTML +=
-                                        final[page * recordsPerPage - $i] ;
+                                        final[page * recordsPerPage - $i];
                                 }
                             }
                         }
                     }).on('page', function (event, page) {
                         // console.info(page + ' (from event listening)');
                     });
-
                 }
             });
         });
--- search.php
+++ search.php
@@ -2,24 +2,24 @@
 <?php require_once("./includes/nav.php"); ?>
 <?php require_once("./includes/class/searchProcessor.php"); ?>
 <?php require_once('./includes/class/showAllVideo.php'); ?>
-<?php $search = new searchProcessor($conn);?>
-<?php $showAllVideo = new showAllVideo($conn);?>
-
+<?php $search = new searchProcessor($conn); ?>
+<?php $showAllVideo = new showAllVideo($conn); ?>
 <link rel="stylesheet" href="./assets/css/search.css">
 <main class="main-section-container" id="main">
     <div id="hot_keyword"></div>
-    <button class="search-show-hide btn btn-outline-dark" >Advanced Search</button>
+    <button class="search-show-hide btn btn-outline-dark">Advanced Search</button>
     <div id="advanced-search-container" style="display:none;">
-<!--        <div id="advanced-search-container" >-->
-            <div id="first_row">
-                <label for="videoTitle">Video Title</label>
-                <input type="text" id="videoTitle" placeholder="keyword" size="10">
-                <label for="upload_by">Upload By:</label>
-                <input type="text" id="upload_by" placeholder="User Name" size="10">
-                <label for="description">Description:</label>
-                <input type="text" id="description" placeholder="keyword" size="10">
-             <div class="btn-group">
-                <button type="button" id="category" class="btn btn-outline-dark dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <!--        <div id="advanced-search-container" >-->
+        <div id="first_row">
+            <label for="videoTitle">Video Title</label>
+            <input type="text" id="videoTitle" placeholder="keyword" size="10">
+            <label for="upload_by">Upload By:</label>
+            <input type="text" id="upload_by" placeholder="User Name" size="10">
+            <label for="description">Description:</label>
+            <input type="text" id="description" placeholder="keyword" size="10">
+            <div class="btn-group">
+                <button type="button" id="category" class="btn btn-outline-dark dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
                     Category
                 </button>
                 <div class="dropdown-menu" id="categoryList">
@@ -29,53 +29,57 @@
                     ?>
                 </div>
             </div>
-            </div><P>
+        </div>
+        <P>
             <label for="file_size_min">File_size (between 0 MB and 500 MB):</label>
-            Min:<input type="number" id="file_size_min" name="file_size_min" placeholder="0" min="0" max="499" SIZE="10">
-            Max:<input type="number" id="file_size_max" name="file_size_max" placeholder="500" min="1" max="500" SIZE="10">
-            </P><P>
+            Min:<input type="number" id="file_size_min" name="file_size_min" placeholder="0" min="0" max="499"
+                       SIZE="10">
+            Max:<input type="number" id="file_size_max" name="file_size_max" placeholder="500" min="1" max="500"
+                       SIZE="10">
+        </P>
+        <P>
             <label for="duration_min">Duration (between 0 and 120 Minute):</label>
             Min:<input type="number" id="duration_min" name="duration_min" placeholder="0" min="0" max="119" SIZE="10">
-            Max:<input type="number" id="duration_max" name="duration_max" placeholder="120"  min="1" max="120" SIZE="10">
-            </P><P>
+            Max:<input type="number" id="duration_max" name="duration_max" placeholder="120" min="1" max="120"
+                       SIZE="10">
+        </P>
+        <P>
             <label for="views_min">Views (between 0 and 500 Views):</label>
             Min:<input type="number" id="views_min" name="views_min" placeholder="0" min="0" max="499" SIZE="10">
             Max:<input type="number" id="views_max" name="views_max" placeholder="500" min="1" max="500" SIZE="10">
-            </P><P>
+        </P>
+        <P>
             <label for="uplodate">Upload Date:</label>
-<!--            <input type="datetime-local" id="uplodate_start" name="uplodate_start" >-->
-<!--            <input type="datetime-local" id="uplodate_end" name="uplodate_end" >-->
-                <input type="datetime-local" id="uplodate_start" name="uplodate_start" value='2020-03-28T08:00' >
-                <input type="datetime-local" id="uplodate_end" name="uplodate_end" value='2020-04-13T08:00' >
-            </P>
-            <input type="submit" class="btn btn-outline-info" id="advancedSearch" name = "advancedSearch">
-
-        </div>
-<!--    <div id="main-content-container" class="paddingTop" >-->
-    <div id="main-content-container" >
+            <!--            <input type="datetime-local" id="uplodate_start" name="uplodate_start" >-->
+            <!--            <input type="datetime-local" id="uplodate_end" name="uplodate_end" >-->
+            <input type="datetime-local" id="uplodate_start" name="uplodate_start" value='2020-03-28T08:00'>
+            <input type="datetime-local" id="uplodate_end" name="uplodate_end" value='2020-04-13T08:00'>
+        </P>
+        <input type="submit" class="btn btn-outline-info" id="advancedSearch" name="advancedSearch">
+    </div>
+    <!--    <div id="main-content-container" class="paddingTop" >-->
+    <div id="main-content-container">
         <hr style="width: 100%; height: 1px; border: none; background-color: #282828">
         <div id="search_result"></div>
         <div id="page-nav">
             <nav aria-label="Page navigation">
-            <ul class="pagination" id="pagination"></ul>
+                <ul class="pagination" id="pagination"></ul>
             </nav>
         </div>
     </div>
 </main>
 <script>
-
     $(document).ready(function () {
         //    change search textbox text
-        var keyword= '<?php echo $_GET['search_input']?>';
-        var category ='';
-        document.getElementById("search_input").value= keyword;
+        var keyword = '<?php echo $_GET['search_input']?>';
+        var category = '';
+        document.getElementById("search_input").value = keyword;
         // document.getElementById("search_input").setAttribute("value",keyword);
         // real time catch keyword in input box
-        $("#search_input").on("input", function(){
+        $("#search_input").on("input", function () {
             keyword = $(this).val();
         });
-
-        //advanced search div
+//advanced search div
         $(".search-show-hide").on("click", function () {
             let main = $("#main-content-container");
             let search = $("#advanced-search-container");
@@ -88,11 +92,9 @@
             main.toggleClass("paddingTop");
         });
         //category list dropdown button
-        $('#categoryList a').on('click', function() {
+        $('#categoryList a').on('click', function () {
             category = ($(this).text());
-
             document.getElementById("category").innerHTML = category;
-
         });
         //advancedsearch button click
         $('#advancedSearch').on("click", function () {
@@ -107,130 +109,109 @@
             var videoTitle = document.getElementById("videoTitle").value;
             var upload_by = document.getElementById("upload_by").value;
             var description = document.getElementById("description").value;
-
-            if ( (parseInt(file_size_min) > parseInt(file_size_max)) || (parseInt(duration_min) > parseInt(duration_max)) ){
+            if ((parseInt(file_size_min) > parseInt(file_size_max)) || (parseInt(duration_min) > parseInt(duration_max))) {
                 alert('The min value cannot larger than max value');
-        }
-            else if ( (parseInt(file_size_min) == parseInt(file_size_max)) || (parseInt(duration_min) == parseInt(duration_max)) ){
+            } else if ((parseInt(file_size_min) == parseInt(file_size_max)) || (parseInt(duration_min) == parseInt(duration_max))) {
                 alert('The min value cannot equal to max value');
-            }
-            else {
-            document.getElementById("search_result").innerHTML="";
-            $.ajax({
-                type:'POST',
-                url:'searchprocess.php',
-                data:{
-                    advancedSearch:"1",
-                    search_input:keyword,
-                    file_size_min:file_size_min,
-                    file_size_max:file_size_max,
-                    duration_min:duration_min,
-                    duration_max:duration_max,
-                    views_min:views_min,
-                    views_max:views_max,
-                    uplodate_start:uplodate_start,
-                    uplodate_end:uplodate_end,
-                    category:category,
-                    videoTitle:videoTitle,
-                    upload_by:upload_by,
-                    description:description
-                },
-                datatype:'json',
-                success:function(result){
-                    final2 = JSON.parse(result);
-
-                    datalength = final2.length;
-                    if (datalength != 0){
-                        $('#pagination').twbsPagination('destroy');
-                        window.pagObj = $('#pagination').twbsPagination({
-                            totalPages: (datalength % 4) ?  (datalength /4) + 1: datalength /4,
-                            visiblePages: 5,
-                            onPageClick: function (event, page) {
-
-                                document.getElementById("search_result").innerHTML="";
-                                for ($i = 4; $i >0 ; $i--) {
-                                    if ( final2[page * 4 - $i]){
-
-                                        document.getElementById("search_result").innerHTML += final2[page * 4 - $i] ;
+            } else {
+                document.getElementById("search_result").innerHTML = "";
+                $.ajax({
+                    type: 'POST',
+                    url: 'searchprocess.php',
+                    data: {
+                        advancedSearch: "1",
+                        search_input: keyword,
+                        file_size_min: file_size_min,
+                        file_size_max: file_size_max,
+                        duration_min: duration_min,
+                        duration_max: duration_max,
+                        views_min: views_min,
+                        views_max: views_max,
+                        uplodate_start: uplodate_start,
+                        uplodate_end: uplodate_end,
+                        category: category,
+                        videoTitle: videoTitle,
+                        upload_by: upload_by,
+                        description: description
+                    },
+                    datatype: 'json',
+                    success: function (result) {
+                        final2 = JSON.parse(result);
+                        datalength = final2.length;
+                        if (datalength != 0) {
+                            $('#pagination').twbsPagination('destroy');
+                            window.pagObj = $('#pagination').twbsPagination({
+                                totalPages: (datalength % 4) ? (datalength / 4) + 1 : datalength / 4,
+                                visiblePages: 5,
+                                onPageClick: function (event, page) {
+                                    document.getElementById("search_result").innerHTML = "";
+                                    for ($i = 4; $i > 0; $i--) {
+                                        if (final2[page * 4 - $i]) {
+                                            document.getElementById("search_result").innerHTML += final2[page * 4 - $i];
+                                        }
                                     }
                                 }
-
-                            }
-                        }).on('page', function (event, page) {
-
-                        });
+                            }).on('page', function (event, page) {
+                            });
+                        } else {
+                            document.getElementById("search_result").innerText = "No matched Videos";
+                        }
                     }
-                    else{
-                        document.getElementById("search_result").innerText ="No matched Videos";
-                    }
-                }
-            })
+                })
             }
         });
-
-    //normal search result based on keyword empty or not
-    //     if( document.getElementById("search_input").value ==''){
-    //         document.getElementById("search_result").innerText='You didn\'t input any keyword';
-    //     }
-    //     else{
-
-    // normal search page plugin
+//normal search result based on keyword empty or not
+        //     if( document.getElementById("search_input").value ==''){
+        //         document.getElementById("search_result").innerText='You didn\'t input any keyword';
+        //     }
+        //     else{
+// normal search page plugin
         $.ajax({
-            type:'POST',
-            url:'searchprocess.php',
-            data:{
-                normalSearch:"1",
-                search_input:keyword
-
+            type: 'POST',
+            url: 'searchprocess.php',
+            data: {
+                normalSearch: "1",
+                search_input: keyword
             },
-            datatype:'json',
-            success:function(result){
+            datatype: 'json',
+            success: function (result) {
                 final1 = JSON.parse(result);
-
                 datalength = final1.length;
-                if (datalength != 0){
+                if (datalength != 0) {
                     $('#pagination').twbsPagination('destroy');
                     window.pagObj = $('#pagination').twbsPagination({
-                        totalPages: (datalength % 4) ?  (datalength /4) + 1: datalength /4,
+                        totalPages: (datalength % 4) ? (datalength / 4) + 1 : datalength / 4,
                         visiblePages: 5,
                         onPageClick: function (event, page) {
-                            document.getElementById("search_result").innerHTML="";
-
-                            for ($i = 4; $i >0 ; $i--) {
-                                if ( final1[page * 4 - $i]){
-
-                                    document.getElementById("search_result").innerHTML += final1[page * 4 - $i] ;
+                            document.getElementById("search_result").innerHTML = "";
+                            for ($i = 4; $i > 0; $i--) {
+                                if (final1[page * 4 - $i]) {
+                                    document.getElementById("search_result").innerHTML += final1[page * 4 - $i];
                                 }
                             }
-
                         }
                     }).on('page', function (event, page) {
-
                     });
-                }
-                else{
-                    document.getElementById("search_result").innerHTML ="No matched Videos";
+                } else {
+                    document.getElementById("search_result").innerHTML = "No matched Videos";
                 }
             }
         });
         // }   normal search result based on keyword empty or not end
-    //    fetch hot keyword data
+        //    fetch hot keyword data
         $.ajax({
-            type:'POST',
-            url:'searchprocess.php',
-            data:{
-                hotkeyword:"1",
+            type: 'POST',
+            url: 'searchprocess.php',
+            data: {
+                hotkeyword: "1",
             },
-            datatype:'json',
-            success:function(result) {
+            datatype: 'json',
+            success: function (result) {
                 final3 = JSON.parse(result);
-
-                document.getElementById("hot_keyword").innerHTML= "Popular Keyword:&nbsp";
-                $.each(final3, function( index, value ) {
+                document.getElementById("hot_keyword").innerHTML = "Popular Keyword:&nbsp";
+                $.each(final3, function (index, value) {
                     document.getElementById("hot_keyword").innerHTML += value;
                 });
-
-
             }
         });
     });
